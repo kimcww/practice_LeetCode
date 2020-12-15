96b98840a251722406ec93d74db01839a4fcc17c
kimchangwoo <42766429+kimcww@users.noreply.github.com>
2020-12-15 13:17:58 +0000

    Sync LeetCode submission - Goal Parser Interpretation (cpp)

--- problems/goal_parser_interpretation/solution.cpp
+++ problems/goal_parser_interpretation/solution.cpp
@@ -4,12 +4,15 @@ public:
         string ret;
         for (int i = 0 ; i < command.size(); i++)
         {
-            if (command[i] == '(' || command[i] == ')')
+            if (command[i] == '(')
             {
                 if (command[i + 1] == ')')
                     ret += 'o';
                 continue;
             }
+            else if(command[i] == ')')
+                continue;
+            
             ret += command[i];
         }
         return ret;
